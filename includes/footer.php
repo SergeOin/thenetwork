19a83ab3e949ec9e81a4097118152c0025587b6c
Enzo <enzo.bodingermon@ecoles-wis.net>
2019-12-09 11:02:18 +0100

    fix: url

--- includes/footer.php
+++ includes/footer.php
@@ -5,8 +5,8 @@
     </div>
 </footer>
 </div>
-<script src="../public/js/manifest.js"></script>
-<script src="../public/js/vendor.js"></script>
-<script src="../public/js/app.js"></script>
+<script src="./public/js/manifest.js"></script>
+<script src="./public/js/vendor.js"></script>
+<script src="./public/js/app.js"></script>
 </body>
 </html>
