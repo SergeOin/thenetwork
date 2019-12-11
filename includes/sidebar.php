41ce2e2b960a1cdf35c3919227c1b873d748bf67
Enzo <enzo.bodingermon@ecoles-wis.net>
2019-12-11 13:19:41 +0100

    update: sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,7 @@
 <div class="card sidebar">
     <div class="card-body">
         <ul class="nav flex-column">
-            <li class="nav-item"><a class="nav-link h6" href="./profile.php"><?php echo getAuth()['first_name']?> <?php echo getAuth()['last_name']?></a></li>
+            <li class="nav-item mx-auto"><a class="nav-link h6" href="./profile.php"><img class="d-block w-100 rounded-circle mx-auto mb-2" style="max-width: 80px" src="https://resize-parismatch.lanmedia.fr/img/var/news/storage/images/paris-match/people-a-z/michel-drucker/6000670-8-fre-FR/Michel-Drucker.jpg" alt="profile-picture"><?php echo getAuth()['first_name']?> <?php echo getAuth()['last_name']?></a></li>
             <li class="nav-item"><a class="nav-link" href="./index.php">Fil d'actualité</a></li>
             <li class="nav-item"><a class="nav-link" href="./profile.php">Profil</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Amis</a></li>
