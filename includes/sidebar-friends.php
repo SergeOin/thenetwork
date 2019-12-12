01190eb3c0e4f702d2b93bfbb1becd0ba04ce5ae
Enzo <enzo.bodingermon@ecoles-wis.net>
2019-12-12 14:17:42 +0100

    update: sidebar scss

--- includes/sidebar-friends.php
+++ includes/sidebar-friends.php
@@ -1,12 +1,14 @@
 <div class="card sidebar">
-    <div class="card-body">
+    <div class="card-body title">
         <ul class="nav flex-column">
             <li class="nav-item mx-auto">Utilisateurs</li>
         </ul>
     </div>
+    <div class="card-body">
     <?php foreach (getUsers() as $user): ?>
-    <div class="card-footer">
-        <p class="mt-1"><?php echo $user['first_name']." ".$user['last_name']?></p>
+    <div>
+        <p class="mt-auto"><?php echo $user['first_name']." ".$user['last_name']?></p>
     </div>
     <?php endforeach; ?>
+    </div>
 </div>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,8 @@
 <div class="card sidebar">
-    <div class="card-body">
-        <ul class="nav flex-column">
-            <li class="nav-item mx-auto"><a class="nav-link h6" href="./profile.php"><img class="d-block w-100 rounded-circle mx-auto mb-2" style="max-width: 80px" src="https://resize-parismatch.lanmedia.fr/img/var/news/storage/images/paris-match/people-a-z/michel-drucker/6000670-8-fre-FR/Michel-Drucker.jpg" alt="profile-picture"><?php echo getAuth()['first_name']?> <?php echo getAuth()['last_name']?></a></li>
+        <ul class="nav flex-column card-body profile">
+            <li class="nav-item text-center"><a class="nav-link" href="./profile.php"><img class="w-100 mr-2 mb-1 rounded-circle" style="max-width: 30px" src="https://resize-parismatch.lanmedia.fr/img/var/news/storage/images/paris-match/people-a-z/michel-drucker/6000670-8-fre-FR/Michel-Drucker.jpg" alt="profile-picture"><?php echo getAuth()['first_name']?> <?php echo getAuth()['last_name']?></a></li>
+        </ul>
+        <ul class="nav flex-column card-body">
             <li class="nav-item"><a class="nav-link" href="./index.php">Fil d'actualité</a></li>
             <li class="nav-item"><a class="nav-link" href="./profile.php">Profil</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Amis</a></li>
@@ -9,5 +10,4 @@
             <li class="nav-item"><a class="nav-link" href="#">Paramètres</a></li>
             <li class="nav-item"><a class="nav-link" href="./assets/logout.php">Déconnexion</a></li>
         </ul>
-    </div>
 </div>
